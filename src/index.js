b7dd04b005e7d5221ab3743986340b4ea1d1a19e
unaisek <mohammedunaiseknkt@gmail.com>
2024-12-10 00:03:59 +0530

    file upload in chat finished

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ app.use(cors({
   credentials:true,
 }))
 app.use("/uploads/profiles", express.static("uploads/profiles"));
+app.use("/uploads/files", express.static("uploads/files"));
+
 app.use(cookieParser());
 app.use(express.json())
 
--- src/routes/messagesRoute.js
+++ src/routes/messagesRoute.js
@@ -1,10 +1,13 @@
 import { Router } from "express"
 import { verifyToken } from "../middlewares/authMiddleware.js";
-import { getMessages } from "../controllers/messageController.js";
+import { getMessages, uploadFile } from "../controllers/messageController.js";
+import multer from "multer";
 
-const messageRoute =Router()
+const messageRoute =Router();
+const upload = multer({dest:"uploads/files"})
 
 messageRoute.post("/get-messages", verifyToken, getMessages);
+messageRoute.post("/upload-file", verifyToken, upload.single("file"), uploadFile)
 
 
 export default messageRoute
